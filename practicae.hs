9ce0ee686063a425fed6ffaffb5a669b5a4c5376
Elisa Cuevas <elisa.cuefe@ciencias.unam.mx>
2024-08-30 13:46:10 -0600

    Agrego mi parte

--- practicae.hs
+++ practicae.hs
@@ -3,12 +3,17 @@
 distanciaPuntos :: (Double,Double) -> (Double,Double) -> Double
 distanciaPuntos (x1,y1) (x2,y2) = sqrt ((x2-x1)^2 + (y2-y1)^2)
 
-
+{-Ejercicio 2-}
+hipotenusa :: (Float,Float) -> Float
+hipotenusa (x,y) = sqrt ((x^2) + (y^2))
 
 {-Ejercicio 3-}
 pendiente :: (Double,Double) -> (Double,Double) -> Double
 pendiente (x1,y1) (x2,y2) =  (y2-y1) / (x2-x1)
 
+{-Ejercicio 4-}
+raices :: Float -> Float -> Float -> (Float,Float)
+raices a b c = ((-b + sqrt((b^2 - 4 * a * c) * (-1))) / (2 * a), (-b - sqrt ((b^2 - 4 * a * c) * (-1))) / (2 * a))
 
 {-Ejercicio 5-}
 aux_perimetro :: Double -> Double -> Double -> Double
@@ -17,6 +22,15 @@ aux_perimetro a b c =  (a+b+c) / 2
 areaTriangulo :: Double -> Double -> Double -> Double
 areaTriangulo a b c = sqrt ((aux_perimetro a b c)* ((aux_perimetro a b c) - a)*((aux_perimetro a b c) - b)*((aux_perimetro a b c) - c ))
 
+{-Ejercicio 6-}
+comparador :: Int -> Int -> Int
+comparador x y = if x == y
+    then 0
+    else 
+        if x < y
+            then -1 
+            else 1
+
 {-Ejercicio 7-}
 aux_maximo :: Int -> [Int] -> Int
 aux_maximo p [] = p
@@ -24,3 +38,9 @@ aux_maximo p (x:xs) = if p <= x then aux_maximo x xs else aux_maximo p xs
 
 maximo :: Int -> Int -> Int -> Int
 maximo x y z = aux_maximo x ([y,z])
+
+{-Ejercicio 8-}
+esDescendente :: Int -> Int -> Int -> Int -> Bool
+esDescendente x y z w = if x > y && y > z && z > w
+    then True
+    else False
